6fdcb3ae12f5ef97e44461fb3d662524f7ef33f1
unknown <alexmanolache92@yahoo.com>
2023-10-10 22:18:01 +0300

    add a local todo list and create a header component

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, FlatList} from 'react-native';
+import Header from './components/header';
 
 export default function App() {
+
+  const [todos, setTodos] = useState ([
+    {text: 'buy a coffee', key: '1'},
+    {text: 'buy some pizza', key: '2'},
+    {text: 'learn how to cook pasta', key: '3'}
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>App has been installed!</Text>
-      <StatusBar style="auto" />
+       <Header />
+       <View style={styles.content}>
+          {/* to do form */}
+          <View style={styles.list}>
+            <FlatList  
+              data={todos}
+              renderItem={({ item }) => (
+                <Text>{ item.text }</Text>
+              )}
+            />
+          </View>
+       </View>
+
     </View>
   );
 }
@@ -14,7 +33,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  content: {
+   padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  }
 });
